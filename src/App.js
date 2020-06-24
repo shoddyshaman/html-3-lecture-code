81ab471bac98a738d66ef3fd0c6da43f830680c9
mattcbodily <mattcbodily@gmail.com>
2020-06-24 09:32:19 -0600

    boiler-plate setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <header className='main-header'>
+          <img src='https://v.fastcdn.co/u/420f55dd/28913912-0-dm-white-logo-2.png' alt='DevMountain'/>
+          <nav>
+            <span>Home</span>
+            <span>About</span>
+            <span>Contact</span>
+          </nav>
+        </header>
+        <section className='animation-transition'>
+          <img className='react-logo' src={logo} alt='React.js'/>
+          <div className='transition-circle'>Hover your mouse over me!</div>
+        </section>
+      </div>
+    )
+  }
 }
 
 export default App;
