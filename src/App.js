7e925b0809220623a4f4ff028aae9730d30ec9c5
mattcbodily <mattcbodily@gmail.com>
2020-06-24 12:21:08 -0600

    finished lecture code

--- src/App.js
+++ src/App.js
@@ -3,16 +3,37 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      dropdownView: false
+    }
+  }
+
+  toggleDropdown = () => {
+    this.setState({dropdownView: !this.state.dropdownView})
+  }
+
   render(){
     return (
       <div className="App">
         <header className='main-header'>
           <img src='https://v.fastcdn.co/u/420f55dd/28913912-0-dm-white-logo-2.png' alt='DevMountain'/>
-          <nav>
+          <nav className='desktop-menu'>
             <span>Home</span>
             <span>About</span>
             <span>Contact</span>
           </nav>
+          <div className='dropdown' onClick={this.toggleDropdown}>Menu</div>
+          {this.state.dropdownView
+            ? (
+              <nav className='mobile-menu'>
+                <span>Home</span>
+                <span>About</span>
+                <span>Contact</span>
+              </nav>
+            )
+            : null}
         </header>
         <section className='animation-transition'>
           <img className='react-logo' src={logo} alt='React.js'/>
